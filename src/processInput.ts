237cce07111d5710073b433fa1554380ae999d15
a_wallis <a_wallis@wargaming.net>
2020-11-13 19:33:37 +0000

    Fixed bug that Array Schemas weren't skippable
    Fixed error in documentation

--- src/processInput.ts
+++ src/processInput.ts
@@ -49,107 +49,113 @@ function recursiveCheck(
 		if (schemaKeys.includes(inputKey)) {
 			// Remove the key from the schema, it won't need to be checked later
 			schemaKeys = schemaKeys.filter((e) => e !== inputKey);
-			// Define the schema key
-			const schemaKey = inputKey;
-			// If the schema key is an array
-			if (Array.isArray(schema[schemaKey])) {
-				// Then we want to handle the array
-				if (!Array.isArray(input[schemaKey])) {
-					response.error = true;
-					response.errors[`${schemaKey}`] = `Expecting an Array of entries but did not receive one`;
-				}
-				else {
-					// Check the inside of the Array Object
-					const potentialKeys = Object.keys(schema[schemaKey][0]);
-					// If the inside of the object includes type
-					// !!!! DOES THIS NOT NEED A [0] ADDED TO THE ISOBJECT CHECK?  HERE!
-					if (potentialKeys.includes(`type`) && !isObject(schema[schemaKey].type)) {
-						// Use this object as the schema and process through each input array value
-						input[schemaKey].forEach((arrayKey, i) => {
-							// Then we need to add any options from this block to local options
-							// Process The input values
-							const processResponse = processInputType(
-								schema[schemaKey][0].type, arrayKey, schemaKey, globalOptions, schema[schemaKey][0],
-							);
-							if (processResponse.error) {
-								response.error = true;
-								// If we don't already have an array to handle the errors
-								if (!Array.isArray(response.errors[schemaKey])) {
-									response.errors[schemaKey] = [];
-								}
-								// Create an Error that uses the array number as the key
-								const newError = { [i]: processResponse.errors[schemaKey] };
-								// Add that to an array of errors
-								response.errors[schemaKey].push(newError);
-							}
-							else if (processResponse.data !== null) {
-								if (response.data[schemaKey] === undefined) { response.data[schemaKey] = []; }
-								response.data[schemaKey].push(processResponse.data);
-							}
-						});
+			// If we aren't supposed to skip this key
+			if (!schema[inputKey]?.skip?.value && !schema[inputKey][0]?.skip?.value) {
+				// Define the schema key
+				const schemaKey = inputKey;
+				// If the schema key is an array
+				if (Array.isArray(schema[schemaKey])) {
+					// Then we want to handle the array
+					if (!Array.isArray(input[schemaKey])) {
+						response.error = true;
+						response.errors[`${schemaKey}`] = `Expecting an Array of entries but did not receive one`;
 					}
 					else {
-						// If there is more depth to the object
-						input[schemaKey].forEach((arrayKey, i) => {
-							// Go in and recursively dive into the objects
-							const recursiveResponse = recursiveCheck(schema[schemaKey][0], arrayKey, globalOptions);
-							let responseKeys = [];
-							if (recursiveResponse.data !== null) {
-								responseKeys = Object.keys(recursiveResponse.data);
-							}
-							if (recursiveResponse.error) {
-								response.error = true;
-								response.errors[`${schemaKey}`] = recursiveResponse.errors;
-							}
-							else if (recursiveResponse.data !== null && responseKeys.length) {
-								if (response.data[schemaKey] === undefined) { response.data[schemaKey] = []; }
-								response.data[schemaKey].push(recursiveResponse.data);
-							}
-						});
+						// Check the inside of the Array Object
+						const potentialKeys = Object.keys(schema[schemaKey][0]);
+						// If the inside of the object includes type
+						// !!!! DOES THIS NOT NEED A [0] ADDED TO THE IS OBJECT CHECK?  HERE!
+						if (potentialKeys.includes(`type`) && !isObject(schema[schemaKey].type)) {
+							// Use this object as the schema and process through each input array value
+							input[schemaKey].forEach((arrayKey, i) => {
+								// Then we need to add any options from this block to local options
+								// Process The input values
+								const processResponse = processInputType(
+									schema[schemaKey][0].type, arrayKey, schemaKey, globalOptions, schema[schemaKey][0],
+								);
+								if (processResponse.error) {
+									response.error = true;
+									// If we don't already have an array to handle the errors
+									if (!Array.isArray(response.errors[schemaKey])) {
+										response.errors[schemaKey] = [];
+									}
+									// Create an Error that uses the array number as the key
+									const newError = { [i]: processResponse.errors[schemaKey] };
+									// Add that to an array of errors
+									response.errors[schemaKey].push(newError);
+								}
+								else if (processResponse.data !== null) {
+									if (response.data[schemaKey] === undefined) { response.data[schemaKey] = []; }
+									response.data[schemaKey].push(processResponse.data);
+								}
+							});
+						}
+						else {
+							// If there is more depth to the object
+							input[schemaKey].forEach((arrayKey, i) => {
+								// Go in and recursively dive into the objects
+								const recursiveResponse = recursiveCheck(schema[schemaKey][0], arrayKey, globalOptions);
+								let responseKeys = [];
+								if (recursiveResponse.data !== null) {
+									responseKeys = Object.keys(recursiveResponse.data);
+								}
+								if (recursiveResponse.error) {
+									response.error = true;
+									response.errors[`${schemaKey}`] = recursiveResponse.errors;
+								}
+								else if (recursiveResponse.data !== null && responseKeys.length) {
+									if (response.data[schemaKey] === undefined) { response.data[schemaKey] = []; }
+									response.data[schemaKey].push(recursiveResponse.data);
+								}
+							});
+						}
 					}
 				}
-			}
-			// If the schema key is an object
-			else if (typeof schema[schemaKey] === `object` && schema[schemaKey] !== null) {
-				// Get the keys of the sub object
-				const subObjKeys = Object.keys(schema[schemaKey]);
-				// If the Schema Key Object includes a type key
-				if (subObjKeys.includes(`type`) && !isObject(schema[schemaKey].type)) {
-					// Then we want to validate the type of the object against the non-nested value
-					const checkResponse = processInputType(
-						schema[schemaKey].type,
-						input[schemaKey],
-						schemaKey,
-						globalOptions,
-						schema[schemaKey],
-					);
-					if (checkResponse.error) {
-						response.error = true;
-						response.errors = Object.assign(response.errors, checkResponse.errors);
+				// If the schema key is an object
+				else if (typeof schema[schemaKey] === `object` && schema[schemaKey] !== null) {
+					// Get the keys of the sub object
+					const subObjKeys = Object.keys(schema[schemaKey]);
+					// If the Schema Key Object includes a type key
+					if (subObjKeys.includes(`type`) && !isObject(schema[schemaKey].type)) {
+						// Then we want to validate the type of the object against the non-nested value
+						const checkResponse = processInputType(
+							schema[schemaKey].type,
+							input[schemaKey],
+							schemaKey,
+							globalOptions,
+							schema[schemaKey],
+						);
+						if (checkResponse.error) {
+							response.error = true;
+							response.errors = Object.assign(response.errors, checkResponse.errors);
+						}
+						else if (checkResponse.data !== null) {
+							if (response.data === null) { response.data = {}; }
+							response.data[schemaKey] = checkResponse.data;
+						}
+	
 					}
-					else if (checkResponse.data !== null) {
-						if (response.data === null) { response.data = {}; }
-						response.data[schemaKey] = checkResponse.data;
+					// Otherwise
+					else {
+						// we want to deep dive
+						const deepResponse = recursiveCheck(schema[schemaKey], input[schemaKey], globalOptions);
+						if (deepResponse.error) {
+							response.error = true;
+							response.errors[schemaKey] = deepResponse.errors;
+						}
+						else if (deepResponse.data !== null) {
+							response.data[schemaKey] = deepResponse.data;
+						}
 					}
-
 				}
-				// Otherwise
+				// Otherwise, it isn't a type identifier, it isn't an array, and it isn't an object to check
 				else {
-					// we want to deep dive
-					const deepResponse = recursiveCheck(schema[schemaKey], input[schemaKey], globalOptions);
-					if (deepResponse.error) {
-						response.error = true;
-						response.errors[schemaKey] = deepResponse.errors;
-					}
-					else if (deepResponse.data !== null) {
-						response.data[schemaKey] = deepResponse.data;
-					}
+					// Then we want to throw an error
+					throw Error(`UNEXPECTED ERROR`);
 				}
 			}
-			// Otherwise, it isn't a type identifier, it isn't an array, and it isn't an object to check
 			else {
-				// Then we want to throw an error
-				throw Error(`UNEXPECTED ERROR`);
+				response.data[inputKey] = input[inputKey]
 			}
 		}
 		// If we don't need to get rid of the extra inputs, add them to the response
